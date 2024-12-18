57e98cfbe9ba5fa8816f3b25d99b0934c0d97e81
te-ing <hi2177@naver.com>
2024-12-18 16:51:43 +0900

    server: public image 접근 가능하도록 serve-static 구현

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,12 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PageModule } from './page/page.module';
 import { UploadModule } from './upload/upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/wemb'),
     PageModule,
     UploadModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
