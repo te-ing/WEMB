a19c96cf6602b7e55b5228fe8cec7102cb3389f4
te-ing <hi2177@naver.com>
2024-12-18 15:16:04 +0900

    client: Priview div의 background가 아닌 img 태그를 사용하도록 수정

--- client/src/app/create/_mainSection/Preview.tsx
+++ client/src/app/create/_mainSection/Preview.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useFileToImage } from "@/hooks/useFileToImage";
+import { useEffect } from "react";
 
 export default function Preview() {
   const image = useFileToImage();
 
   return (
-    <div
-      className={`w-full h-full bg-cover bg-no-repeat`}
-      style={{ backgroundImage: `url(${image?.src || ""})` }}
-    ></div>
+    <article className="relative flex justify-center">
+      <img src={image?.src} className={`absolute -z-10`}></img>
+    </article>
   );
 }
