9ebc0e979bae324ed185d39b241cad2fb5a286f7
te-ing <hi2177@naver.com>
2024-12-21 17:32:43 +0900

    client: 페이지 생성 시 로딩 및 생성된 페이지로 이동되도록 구현

--- client/src/api/upload.ts
+++ client/src/api/upload.ts
@@ -1,8 +1,9 @@
 "use server";
 
+import { PageData } from "@/type/page.type";
 import { revalidatePath } from "next/cache";
 
-export const postCreatePage = async (formData: FormData) => {
+export const postCreatePage = async (formData: FormData): Promise<PageData> => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/page/create`, {
     method: "POST",
     body: formData,
--- client/src/app/create/_rightSection/CreateButton.tsx
+++ client/src/app/create/_rightSection/CreateButton.tsx
@@ -1,26 +1,36 @@
 import { postCreatePage } from "@/api/upload";
 import { AlertButton } from "@/components/common/AlertButton";
+import { AlertLoading } from "@/components/common/AlertLoading";
 import { useSourceStore } from "@/stores/sourceStore";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import { useStore } from "zustand";
 
 export const CreateButton = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const { title, background } = useStore(useSourceStore);
 
   const createPage = async () => {
     const formData = new FormData();
     formData.append("name", title);
     formData.append("file", background);
-    await postCreatePage(formData);
+    setIsLoading(true);
+    const res = await postCreatePage(formData);
+    router.push(`/page/${res._id}`);
+    setIsLoading(false);
   };
 
   return (
-    <AlertButton
-      dialogTitle={`"${title}"\n페이지를 생성하시겠습니까?`}
-      onConfirm={() => createPage()}
-      triggerTitle="페이지 만들기"
-      triggerClassName={`bg-white border w-fit`}
-      disabled={!title}
-    ></AlertButton>
+    <>
+      {isLoading && <AlertLoading />}
+      <AlertButton
+        dialogTitle={`"${title}"\n페이지를 생성하시겠습니까?`}
+        onConfirm={() => createPage()}
+        triggerTitle="페이지 만들기"
+        triggerClassName={`bg-white border w-fit`}
+        disabled={!title}
+      ></AlertButton>
+    </>
   );
 };
